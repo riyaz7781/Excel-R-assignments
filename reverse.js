2293e748ff8ace1050b765632e1e1b93347c4914
riyaz7781 <134072805+riyaz7781@users.noreply.github.com>
2024-02-27 21:19:49 +0530

    Delete reverse.js

--- reverse.js
+++
@@ -1,9 +0,0 @@
-const str = 'Hello World how is it outside';
-const reverseSentence = str => {
-   const arr = str.split(" ");
-   const reversed = arr.map(el => {
-      return el.split('').reverse().join("");
-   });
-   return reversed.join(" ");
-};
-console.log(reverseSentence(str));
